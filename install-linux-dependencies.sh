53fca1b66e04ffa2d8123cdb9c8b584f97de389c
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2023-12-01 14:22:45 -0300

    adicionando dependência do python3-dev

--- install-linux-dependencies.sh
+++ install-linux-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y \
-    build-essential python3 curl libwayland-dev libxkbcommon-dev \
-    g++ pkg-config libx11-dev libasound2-dev libudev-dev
+    build-essential python3 python3-dev g++ pkg-config curl \
+    libx11-dev libasound2-dev libudev-dev libwayland-dev libxkbcommon-dev
     
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
